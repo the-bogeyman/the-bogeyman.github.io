06191b92e7634226d3f7320d808c20cb9f1065e5
the-bogeyman <b45b26@gmail.com>
2016-12-19 01:11:39 +0600

    changed some info

--- fb_app/cred.php
+++ fb_app/cred.php
@@ -1,5 +1,5 @@
 <?php
 $_YOUR_APP_ID = '342552066129153';
-$_YOUR_APP_SECRET = 'XYZ';
-$callback_url = '';  // example.com/login.php
+$_YOUR_APP_SECRET = 'b2238368b0d8ca9ebc441ae46e91604c';
+$callback_url = 'the-bogeyman.github.io/fb_app/login.php';  // example.com/login.php
 ?>
