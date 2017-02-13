f7e3fa0ef6039cb92328cec1057d284e671e4398
the-bogeyman <b45b26@gmail.com>
2017-02-13 19:51:02 +0600

    Cleaned unwanted files

--- fb_app/cred.php
+++
@@ -1,5 +0,0 @@
-<?php
-$_YOUR_APP_ID = '342552066129153';
-$_YOUR_APP_SECRET = 'b2238368b0d8ca9ebc441ae46e91604c';
-$callback_url = 'the-bogeyman.github.io/fb_app/login.php';  // example.com/login.php
-?>
